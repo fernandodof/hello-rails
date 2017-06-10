97b44eac143e7fc4ec08b442d544165a359a0199
Fernando <fernandodof@gmail.com>
2017-06-10 18:17:46 -0300

    Customazing code generation

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610164547) do
+ActiveRecord::Schema.define(version: 20170610205135) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
     t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
